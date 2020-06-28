0cb75b6e5fda47599d5de71e0ecde320f13a10d0
wongdu <412824500@qq.com>
2020-06-28 11:15:36 +0800

    格式化代码

--- ga/module/server-live555/ga-liveserver.cpp
+++ ga/module/server-live555/ga-liveserver.cpp
@@ -11,10 +11,11 @@
 
 static UsageEnvironment* env = NULL;
 static OnDemandServerMediaSubsession* pSubsession;
+static const int subtypeRtcpApp = 10;
 
 void *
 liveserver_taskscheduler() {
-	if(env == NULL)
+	if (env == NULL)
 		return NULL;
 	return &env->taskScheduler();
 }
@@ -42,45 +43,46 @@ liveserver_main(void *arg) {
 		exit(1);
 	}
 	//
-	encoder_pktqueue_init(VIDEO_SOURCE_CHANNEL_MAX+1, 3 * 1024* 1024/*3MB*/);
+	encoder_pktqueue_init(VIDEO_SOURCE_CHANNEL_MAX + 1, 3 * 1024 * 1024/*3MB*/);
 	//
 	ServerMediaSession * sms
 		= ServerMediaSession::createNew(*env,
-				rtspconf->object[0] ? &rtspconf->object[1] : "ga",
-				rtspconf->object[0] ? &rtspconf->object[1] : "ga",
-				rtspconf->title[0] ? rtspconf->title : "GamingAnywhere Server");
+		rtspconf->object[0] ? &rtspconf->object[1] : "ga",
+		rtspconf->object[0] ? &rtspconf->object[1] : "ga",
+		rtspconf->title[0] ? rtspconf->title : "GamingAnywhere Server");
 	//
 	qos_server_init();
 	// add video session
-	if((m = encoder_get_vencoder()) == NULL) {
+	if ((m = encoder_get_vencoder()) == NULL) {
 		ga_error("live-server: FATAL - no video encoder registered.\n");
 		exit(-1);
 	}
-	if(m->mimetype == NULL) {
+	if (m->mimetype == NULL) {
 		ga_error("live-server: FATAL - video encoder does not configure mimetype.\n");
 		exit(-1);
 	}
-	for(cid = 0; cid < video_source_channels(); cid++) {
+	for (cid = 0; cid < video_source_channels(); cid++) {
 		pSubsession = GAMediaSubsession::createNew(*env, cid, m->mimetype);
 		sms->addSubsession(pSubsession);
 	}
 	// add audio session, if necessary
-	if((m = encoder_get_aencoder()) != NULL) {
-		if(m->mimetype == NULL) {
+	if ((m = encoder_get_aencoder()) != NULL) {
+		if (m->mimetype == NULL) {
 			ga_error("live-server: FATAL - audio encoder does not configure mimetype.\n");
 			exit(-1);
 		}
-		sms->addSubsession(GAMediaSubsession::createNew(*env, cid, m->mimetype)); 
+		sms->addSubsession(GAMediaSubsession::createNew(*env, cid, m->mimetype));
 	}
 	rtspServer->addServerMediaSession(sms);
 
-	if(rtspServer->setUpTunnelingOverHTTP(80)
-	|| rtspServer->setUpTunnelingOverHTTP(8000)
-	|| rtspServer->setUpTunnelingOverHTTP(8080)) {
+	if (rtspServer->setUpTunnelingOverHTTP(80)
+		|| rtspServer->setUpTunnelingOverHTTP(8000)
+		|| rtspServer->setUpTunnelingOverHTTP(8080)) {
 		ga_error("(Use port %d for optional RTSP-over-HTTP tunneling.)\n",
 			rtspServer->httpServerPortNum());
 		//*env << "\n(We use port " << rtspServer->httpServerPortNum() << " for optional RTSP-over-HTTP tunneling.)\n";
-	} else {
+	}
+	else {
 		ga_error("(RTSP-over-HTTP tunneling is not available.)\n");
 		//*env << "\n(RTSP-over-HTTP tunneling is not available.)\n";
 	}
@@ -95,7 +97,7 @@ liveserver_main(void *arg) {
 
 //////// qos report functions
 
-static std::map<RTPSink*, std::map<unsigned/*SSRC*/,qos_server_record_t> > sinkmap;
+static std::map<RTPSink*, std::map<unsigned/*SSRC*/, qos_server_record_t> > sinkmap;
 static TaskToken qos_task = NULL;
 static int qos_started = 0;
 static struct timeval qos_tv;
@@ -107,24 +109,24 @@ static void qos_server_schedule();
 static void
 qos_server_report(void *clientData) {
 	struct timeval now;
-	std::map<RTPSink*, std::map<unsigned,qos_server_record_t> >::iterator mi;
-	//
+	std::map<RTPSink*, std::map<unsigned, qos_server_record_t> >::iterator mi;
+
 	gettimeofday(&now, NULL);
-	for(mi = sinkmap.begin(); mi != sinkmap.end(); mi++) {
+	for (mi = sinkmap.begin(); mi != sinkmap.end(); mi++) {
 		RTPTransmissionStatsDB& db = mi->first->transmissionStatsDB();
 		RTPTransmissionStatsDB::Iterator statsIter(db);
 		RTPTransmissionStats *stats = NULL;
-		while((stats = statsIter.next()) != NULL) {
+		while ((stats = statsIter.next()) != NULL) {
 			unsigned ssrc = stats->SSRC();
-			std::map<unsigned,qos_server_record_t>::iterator mj;
+			std::map<unsigned, qos_server_record_t>::iterator mj;
 			unsigned long long pkts_lost, d_pkt_lost;
 			unsigned long long pkts_sent, d_pkt_sent;
 			unsigned long long bytes_sent, d_byte_sent;
 			unsigned pkts_sent_hi, pkts_sent_lo;
 			unsigned bytes_sent_hi, bytes_sent_lo;
 			long long elapsed;
-			//
-			if((mj = mi->second.find(ssrc)) == mi->second.end()) {
+
+			if ((mj = mi->second.find(ssrc)) == mi->second.end()) {
 				qos_server_record_t qr;
 				bzero(&qr, sizeof(qr));
 				qr.timestamp = now;
@@ -132,30 +134,21 @@ qos_server_report(void *clientData) {
 				continue;
 			}
 
-			//if (pSubsession){
+			//只有视频RTPSink类型，才回传rtd往返时延
 			if ((strcmp("H264", mi->first->rtpPayloadFormatName()) == 0 ||
-				 strcmp("H265", mi->first->rtpPayloadFormatName()) == 0) && 
-				 pSubsession){
-				//std::string strTemp = std::to_string(1000.0 * stats->roundTripDelay() / 65536);		
-				//std::string strTemp = "12345678";
-				//pSubsession->sendRTCPAppPacket(111, "roundTripDelay", (unsigned char*)strTemp.c_str(), sizeof(strTemp.c_str()));
-				//pSubsession->sendRTCPAppPacket(117, "roundTripDelay", (unsigned char*)strTemp.c_str(), strTemp.length());
-				/*pSubsession->sendRTCPAppPacket(117, "ab", (unsigned char*)strTemp.c_str(), strTemp.length());
-				ga_error("ssrc (%lu),roundTripDelay (%s) (%d)  (%x)\n", ssrc, strTemp.c_str(), strTemp.length(),stats);*/
-				//memset(arrFeedbackRtd, '\0', Max_Rtd_Length);
+				strcmp("H265", mi->first->rtpPayloadFormatName()) == 0) &&
+				pSubsession){
 				memset(arrFeedbackRtd, 0, Max_Rtd_Length);
-				//sprintf_s(arrFeedbackRtd, "%u_%s", ssrc, std::to_string(1000.0 * stats->roundTripDelay() / 65536).c_str());
-				//sprintf(arrFeedbackRtd, "%u_%s", ssrc, std::to_string(1000.0 * stats->roundTripDelay() / 65536).c_str());
 				sprintf(arrFeedbackRtd, "%u_%f", ssrc, 1000.0 * stats->roundTripDelay() / 65536);
-				pSubsession->sendRTCPAppPacket(10, "ab", (unsigned char*)arrFeedbackRtd, strlen(arrFeedbackRtd));
+				pSubsession->sendRTCPAppPacket(subtypeRtcpApp, "videoRTD", (unsigned char*)arrFeedbackRtd, strlen(arrFeedbackRtd));
 				ga_error("ssrc (%lu),roundTripDelay (%s) (%d)  (%x)\n", ssrc, (unsigned char*)arrFeedbackRtd, strlen(arrFeedbackRtd), stats);
 			}
-			//
+
 			elapsed = tvdiff_us(&now, &mj->second.timestamp);
-			if(elapsed < QOS_SERVER_REPORT_INTERVAL_MS * 1000)
+			if (elapsed < QOS_SERVER_REPORT_INTERVAL_MS * 1000)
 				continue;
 			mj->second.timestamp = now;
-			//
+		
 			pkts_lost = stats->totNumPacketsLost();
 			stats->getTotalPacketCount(pkts_sent_hi, pkts_sent_lo);
 			stats->getTotalOctetCount(bytes_sent_hi, bytes_sent_lo);
@@ -168,24 +161,24 @@ qos_server_report(void *clientData) {
 			d_pkt_sent = pkts_sent - mj->second.pkts_sent;
 			d_byte_sent = bytes_sent - mj->second.bytes_sent;
 			//
-			if(d_pkt_lost > d_pkt_sent) {
+			if (d_pkt_lost > d_pkt_sent) {
 				ga_error("qos: invalid packet loss count detected (%d)\n", d_pkt_lost);
 				d_pkt_lost = 0;
 			}
 			// nothing updated?
-			if(d_pkt_lost == 0 && d_pkt_sent == 0 && d_byte_sent == 0)
+			if (d_pkt_lost == 0 && d_pkt_sent == 0 && d_byte_sent == 0)
 				continue;
 			// report
 			ga_error("%s-report: %lldKB sent; pkt-loss=%lld/%lld,%.2f%%; bitrate=%.0fKbps; rtt=%u (%.3fms); jitter=%u (freq=%uHz)\n",
-					mi->first->rtpPayloadFormatName(),
-					d_byte_sent / 1024,
-					d_pkt_lost, d_pkt_sent, 100.0*d_pkt_lost/d_pkt_sent,
-					(7812.5/*8000000.0/1024*/)*d_byte_sent/elapsed,
-					stats->roundTripDelay(),
-					1000.0 * stats->roundTripDelay() / 65536,
-					stats->jitter(),
-					mi->first->rtpTimestampFrequency());
-			//
+				mi->first->rtpPayloadFormatName(),
+				d_byte_sent / 1024,
+				d_pkt_lost, d_pkt_sent, 100.0*d_pkt_lost / d_pkt_sent,
+				(7812.5/*8000000.0/1024*/)*d_byte_sent / elapsed,
+				stats->roundTripDelay(),
+				1000.0 * stats->roundTripDelay() / 65536,
+				stats->jitter(),
+				mi->first->rtpTimestampFrequency());
+
 			mj->second.pkts_lost = pkts_lost;
 			mj->second.pkts_sent = pkts_sent;
 			mj->second.bytes_sent = bytes_sent;
@@ -200,7 +193,7 @@ qos_server_report(void *clientData) {
 static void
 qos_server_schedule() {
 	struct timeval now, timeout;
-	if(qos_started == 0)
+	if (qos_started == 0)
 		return;
 	timeout.tv_sec = qos_tv.tv_sec;
 	timeout.tv_usec = qos_tv.tv_usec + QOS_SERVER_CHECK_INTERVAL_MS * 1000;
@@ -208,13 +201,13 @@ qos_server_schedule() {
 	timeout.tv_usec %= 1000000;
 	gettimeofday(&now, NULL);
 	qos_task = env->taskScheduler().scheduleDelayedTask(
-			tvdiff_us(&timeout, &now), (TaskFunc*) qos_server_report, NULL);
+		tvdiff_us(&timeout, &now), (TaskFunc*)qos_server_report, NULL);
 	return;
 }
 
 int
 qos_server_start() {
-	if(env == NULL)
+	if (env == NULL)
 		return -1;
 	gettimeofday(&qos_tv, NULL);
 	qos_started = 1;
@@ -230,7 +223,7 @@ qos_server_stop() {
 
 int
 qos_server_add_sink(const char *prefix, RTPSink *rtpsink) {
-	std::map<unsigned/*SSRC*/,qos_server_record_t> x;
+	std::map<unsigned/*SSRC*/, qos_server_record_t> x;
 	sinkmap[rtpsink] = x;
 	ga_error("qos: add sink#%d for %s, rtpsink=%p\n", sinkmap.size(), prefix, rtpsink);
 	return 0;
@@ -244,7 +237,7 @@ qos_server_remove_sink(RTPSink *rtpsink) {
 
 int
 qos_server_deinit() {
-	if(env != NULL) {
+	if (env != NULL) {
 		env->taskScheduler().unscheduleDelayedTask(qos_task);
 	}
 	qos_task = NULL;
@@ -255,7 +248,7 @@ qos_server_deinit() {
 
 int
 qos_server_init() {
-	if(env == NULL) {
+	if (env == NULL) {
 		ga_error("liveserver: UsageEnvironment has not been initialized.\n");
 		return -1;
 	}
