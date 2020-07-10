c44a48a8b0b73cdbbf879a2217ecd1f588f20b73
wongdu <412824500@qq.com>
2020-07-10 11:28:58 +0800

    添加脚本

---
+++ deps.win32/add_d.sh
@@ -0,0 +1,9 @@
+
+pwd
+rm *_d.lib
+mv BasicUsageEnvironment.lib libBasicUsageEnvironment_d.lib
+mv groupsock.lib libgroupsock_d.lib
+mv liveMedia.lib libliveMedia_d.lib
+mv UsageEnvironment.lib libUsageEnvironment_d.lib
+
+#mv *.lib ./lib
